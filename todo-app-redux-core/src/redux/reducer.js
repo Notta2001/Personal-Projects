e0d548dea61d6352017b3e1200f9107bb98a8cee
ThangDoan2001 <doanthang2001@gmail.com>
2022-01-05 10:41:17 +0700

    🌠 To-do App With Redux Core

---
+++ todo-app-redux-core/src/redux/reducer.js
@@ -0,0 +1,18 @@
+import filtersReducer from '../components/Filters/FiltersSlice'
+import todoListReducer from '../components/TodoList/TodosSlice'
+import { combineReducers } from 'redux'
+
+// const rootReducer = (state = {}, action) => {
+//     // console.log(state, action)
+//     return {
+//         filters: filtersReducer(state.filters, action),
+//         todoList: todoListReducer(state.todoList, action)
+//     };
+// };
+
+const rootReducer = combineReducers({
+    filters: filtersReducer,
+    todoList: todoListReducer,
+})
+
+export default rootReducer
